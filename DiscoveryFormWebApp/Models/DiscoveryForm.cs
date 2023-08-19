9509be697216f966350163e0c2662a7f9a9af090
TA <tadams03@mail.roosevelt.edu>
2023-08-19 18:58:08 -0500

    Scaffold Db

---
+++ DiscoveryFormWebApp/Models/DiscoveryForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiscoveryFormWebApp.Models
+{
+    public partial class DiscoveryForm
+    {
+        public int UniqueDiscoveryFormId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public string? VisibilityStatus { get; set; }
+        public string? ApprovalStatus { get; set; }
+        public string? Notes { get; set; }
+        public string? CurrentFlag { get; set; }
+        public DateTime? InsertedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ DiscoveryFormWebApp/Models/DiscoveryFormDBContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DiscoveryFormWebApp.Models
+{
+    public partial class DiscoveryFormDBContext : DbContext
+    {
+        public DiscoveryFormDBContext()
+        {
+        }
+
+        public DiscoveryFormDBContext(DbContextOptions<DiscoveryFormDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<DiscoveryForm> DiscoveryForms { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=ServerName;Database=DiscoveryFormDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DiscoveryForm>(entity =>
+            {
+                entity.HasKey(e => e.UniqueDiscoveryFormId);
+
+                entity.Property(e => e.ApprovalStatus).HasMaxLength(100);
+
+                entity.Property(e => e.Category).HasMaxLength(150);
+
+                entity.Property(e => e.CurrentFlag).HasMaxLength(1);
+
+                entity.Property(e => e.Description).HasMaxLength(1000);
+
+                entity.Property(e => e.InsertedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Notes).HasMaxLength(4000);
+
+                entity.Property(e => e.Title).HasMaxLength(250);
+
+                entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.VisibilityStatus).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
